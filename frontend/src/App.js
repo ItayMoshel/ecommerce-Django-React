9604de6086a4b4a696eedd1bcfd55712a68fd503
itay moshel <itay@itays-MacBook-Pro.local>
2023-01-09 14:43:35 +0200

    Added screens, routes and components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,21 @@
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 function App() {
   return (
-    <div>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-        <h1>
-          Welcome
-        </h1>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} ></Route>
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
